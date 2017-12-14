652f92d0dcfcb0c385b1880dbc1c2e5838ed17c2
Arsen Chystiakov <achystyakov@gmail.com>
2017-12-14 17:56:53 +0200

    Moved carousel to front page

--- wordpress/wp-content/themes/vyo/front-page.php
+++ wordpress/wp-content/themes/vyo/front-page.php
@@ -14,41 +14,74 @@
 
 get_header(); ?>
 
-<div id="primary" class="content-area">
-	<main id="main" class="site-main" role="main">
+    <!-- Featured Tours Carousel -->
+    <div id="carousel-header-cover" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+		
+		<?php
 
-		<?php // Show the selected frontpage content.
-		if ( have_posts() ) :
-			while ( have_posts() ) : the_post();
-				get_template_part( 'template-parts/page/content', 'front-page' );
-			endwhile;
-		else : // I'm not sure it's possible to have no posts when this page is shown, but WTH.
-			get_template_part( 'template-parts/post/content', 'none' );
-		endif; ?>
+		// TODO Mocked function
+		function get_featured_posts() {
+			return $array = array(
+					    0 => array(
+					    	'id' => '10',
+					    	'url' => 'everest',
+					    	'img' => '1.jpg',
+					    	'name' => 'Побачити Еверест',
+					    	'countries' => ['Непал'],
+					    	'date' => '03 квітня - 25 квітня 2018',
+					    	'days' => '10 днів',
+					    	'price' => '885$'
+					    	),
+					    1 => array(
+					    	'id' => '12',
+					    	'url' => 'montblanc',
+					    	'img' => '2.jpg',
+					    	'name' => 'Тур навколо Монблану',
+					    	'countries' => ['Швейцарія', 'Франція', 'Італія'],
+					    	'date' => '03 квітня - 25 квітня 2018',
+					    	'days' => '10 днів',
+					    	'price' => '790€'
+					    	)
+					);
+		}
+
+		$featured_posts = get_featured_posts();
+		foreach ($featured_posts as $i=>$featured_post) {
+		?>
+        <li data-target="#carousel-header-cover" data-slide-to="<?= $i ?>" class="<?= ($i == 0) ? "active" : "" ?>"></li>
+		<?php } ?>
+      </ol>
+      <div class="carousel-inner">
+		<?php
+		foreach ($featured_posts as $i=>$post) {
+		?>
+
+
+        <div class="carousel-item <?= ($i == 0) ? "active" : "" ?>">
+          <img class="d-block w-100" src="wp-content/themes/vyo/assets/images/<?= $post['img'] ?>" alt="First slide">
+          <div class="carousel-caption d-none d-block">
+          	<!-- TODO: Add days wording -->
+            <p class="tour-date font-weight-bold m-2"><?= $post['date'] ?>(<?= $post['days'] ?>)</p>
+            <p class="tour-countries mb-5"><?= join(', ', $post['countries']) ?></p>
+            <h3 class="tour-name mb-4"><a href="#"><?= $post['name'] ?></a></h3>
+            <p class="tour-price font-weight-bold mb-4"><?= $post['price'] ?></p>
+            <button type="button" class="btn btn-outline-vyo-lt mb-4">ВЙО!</button>
+          </div>
+        </div>
 
 		<?php
-		// Get each of our panels and show the post data.
-		if ( 0 !== twentyseventeen_panel_count() || is_customize_preview() ) : // If we have pages to show.
-
-			/**
-			 * Filter number of front page sections in Twenty Seventeen.
-			 *
-			 * @since Twenty Seventeen 1.0
-			 *
-			 * @param int $num_sections Number of front page sections.
-			 */
-			$num_sections = apply_filters( 'twentyseventeen_front_page_sections', 4 );
-			global $twentyseventeencounter;
-
-			// Create a setting and control for each of the sections available in the theme.
-			for ( $i = 1; $i < ( 1 + $num_sections ); $i++ ) {
-				$twentyseventeencounter = $i;
-				twentyseventeen_front_page_section( null, $i );
-			}
-
-	endif; // The if ( 0 !== twentyseventeen_panel_count() ) ends here. ?>
-
-	</main><!-- #main -->
-</div><!-- #primary -->
+		}
+		?>
+      </div>
+      <a class="carousel-control-prev" href="#carousel-header-cover" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Попередній</span>
+      </a>
+      <a class="carousel-control-next" href="#carousel-header-cover" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Наступний</span>
+      </a>
+    </div>
 
 <?php get_footer();
--- wordpress/wp-content/themes/vyo/header.php
+++ wordpress/wp-content/themes/vyo/header.php
@@ -58,77 +58,8 @@ if ( is_front_page() ) {
             </div>
         </div>
     </div>
-
-    <!-- Featured Tours Carousel -->
-    <div id="carousel-header-cover" class="carousel slide" data-ride="carousel">
-      <ol class="carousel-indicators">
-		
-		<?php
-
-		// TODO Mocked function
-		function get_featured_posts() {
-			return $array = array(
-					    0 => array(
-					    	'id' => '10',
-					    	'url' => 'everest',
-					    	'img' => '1.jpg',
-					    	'name' => 'Побачити Еверест',
-					    	'countries' => ['Непал'],
-					    	'date' => '03 квітня - 25 квітня 2018',
-					    	'days' => '10 днів',
-					    	'price' => '885$'
-					    	),
-					    1 => array(
-					    	'id' => '12',
-					    	'url' => 'montblanc',
-					    	'img' => '2.jpg',
-					    	'name' => 'Тур навколо Монблану',
-					    	'countries' => ['Швейцарія', 'Франція', 'Італія'],
-					    	'date' => '03 квітня - 25 квітня 2018',
-					    	'days' => '10 днів',
-					    	'price' => '790€'
-					    	)
-					);
-		}
-
-		$featured_posts = get_featured_posts();
-		foreach ($featured_posts as $i=>$featured_post) {
-		?>
-        <li data-target="#carousel-header-cover" data-slide-to="<?= $i ?>" class="<?= ($i == 0) ? "active" : "" ?>"></li>
-		<?php } ?>
-      </ol>
-      <div class="carousel-inner">
-		<?php
-		foreach ($featured_posts as $i=>$post) {
-		?>
-
-
-        <div class="carousel-item <?= ($i == 0) ? "active" : "" ?>">
-          <img class="d-block w-100" src="wp-content/themes/vyo/assets/images/<?= $post['img'] ?>" alt="First slide">
-          <div class="carousel-caption d-none d-block">
-          	<!-- TODO: Add days wording -->
-            <p class="tour-date font-weight-bold m-2"><?= $post['date'] ?>(<?= $post['days'] ?>)</p>
-            <p class="tour-countries mb-5"><?= join(', ', $post['countries']) ?></p>
-            <h3 class="tour-name mb-4"><a href="#"><?= $post['name'] ?></a></h3>
-            <p class="tour-price font-weight-bold mb-4"><?= $post['price'] ?></p>
-            <button type="button" class="btn btn-outline-vyo-lt mb-4">ВЙО!</button>
-          </div>
-        </div>
-
-		<?php
-		}
-		?>
-      </div>
-      <a class="carousel-control-prev" href="#carousel-header-cover" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Попередній</span>
-      </a>
-      <a class="carousel-control-next" href="#carousel-header-cover" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Наступний</span>
-      </a>
-    </div>
 <?php } ?>
+
 
 
 
