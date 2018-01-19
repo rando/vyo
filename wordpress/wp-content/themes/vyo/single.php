6c8004d259357551b58a880bc23bf4125e2bfb25
Mykola Rykov <mykola.rykov@gmail.com>
2018-01-19 14:13:09 +0200

    Tour page CSSing started; Tour page template added

--- wordpress/wp-content/themes/vyo/single.php
+++ wordpress/wp-content/themes/vyo/single.php
@@ -12,102 +12,105 @@
 
 get_header(); ?>
 
-<div class="wrap">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div class="post-thumbnail">
 
-			<div class="post-thumbnail">
+<?php
+while ( have_posts() ): the_post(); 
+	echo get_post_format();
+	if (get_post_format() == 'gallery') {
+		echo "Gallery";
+	}
+	//tour
+	if (true) {
+		get_template_part( 'post-type-templates/tour' );
+	}
+	//post
+	if (false) {
 
-			<?php
-			while ( have_posts() ): the_post(); 
-				echo get_post_format();
-				if (get_post_format() == 'gallery') {
-					echo "Gallery";
-				}
-				get_template_part( 'template-parts/post/content', get_post_format() );
-			endwhile;
-			?>
+	}
+endwhile;
+?>
 
-			<pre>
-			<?php
-			print_r(get_fields());
-			?>
-			</pre>
+<div class="container" role="main">
 
-				<?php the_post_thumbnail( 'twentyseventeen-featured-image' ); ?>	
-			</div>
-			
-			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	<pre>
+	<?php
+	print_r(get_fields());
+	?>
+	</pre>
 
-			<h2>
-			<?php 
-				if (get_field('discount-price')) {
-			?>
-				<h2><?= get_field('discount-price') ?></h2> (<h2><s><?= the_field('price'); ?></s></h2>)
-			<?php
-				}
-				else {
-			?>
-			<h2><?= get_field('price'); ?></h2>	
-			<?php
-				}
-			?> 
-			<h2><?= get_field('currency'); ?></h2>
-			<h2><?= get_field('guides')->post_title; ?></h2>
-			<h2><?= the_field('countries'); ?></h2>
-			<h2><?= get_field('date-start'); ?></h2>
-			<h2><?= get_field('date-end'); ?></h2>
-			<h2><?= get_field('days-num'); ?></h2>
-			<h2><?= get_field('comfort'); ?></h2>
-			<h2><?= get_field('difficult'); ?></h2>
-			<h2><?= get_field('peoples'); ?></h2>
-			<h2><?= get_field('favorites'); ?></h2>
-			<h2><?= get_field('program'); ?></h2>
-			<h2><?= get_field('included'); ?></h2>
-			<h2><?= get_field('not-included'); ?></h2>
+		<?php the_post_thumbnail( 'twentyseventeen-featured-image' ); ?>	
+	</div>
+	
+	<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
-			<?php
-			print("<pre>".print_r(get_field('gallery'), true)."</pre>");
-			?>
+	<h2>
+	<?php 
+		if (get_field('discount-price')) {
+	?>
+		<h2><?= get_field('discount-price') ?></h2> (<h2><s><?= the_field('price'); ?></s></h2>)
+	<?php
+		}
+		else {
+	?>
+	<h2><?= get_field('price'); ?></h2>	
+	<?php
+		}
+	?> 
+	<h2><?= get_field('currency'); ?></h2>
+	<h2><?= get_field('guides')->post_title; ?></h2>
+	<h2><?= the_field('countries'); ?></h2>
+	<h2><?= get_field('date-start'); ?></h2>
+	<h2><?= get_field('date-end'); ?></h2>
+	<h2><?= get_field('days-num'); ?></h2>
+	<h2><?= get_field('comfort'); ?></h2>
+	<h2><?= get_field('difficult'); ?></h2>
+	<h2><?= get_field('peoples'); ?></h2>
+	<h2><?= get_field('favorites'); ?></h2>
+	<h2><?= get_field('program'); ?></h2>
+	<h2><?= get_field('included'); ?></h2>
+	<h2><?= get_field('not-included'); ?></h2>
 
-			<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum dolore, excepturi quasi vitae esse sint dicta aliquam id quas, temporibus libero amet quaerat alias commodi itaque consectetur eveniet doloremque animi!</h1>
+	<?php
+	print("<pre>".print_r(get_field('gallery'), true)."</pre>");
+	?>
 
-			<?php
-			print("<pre>".print_r(get_field('days'), true)."</pre>");
-			?>
+	<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum dolore, excepturi quasi vitae esse sint dicta aliquam id quas, temporibus libero amet quaerat alias commodi itaque consectetur eveniet doloremque animi!</h1>
 
-		<?php
-		wp_link_pages( array(
-			'before'      => '<div class="page-links">' . __( 'Pages:', 'twentyseventeen' ),
-			'after'       => '</div>',
-			'link_before' => '<span class="page-number">',
-			'link_after'  => '</span>',
-		) );
-		?>
+	<?php
+	print("<pre>".print_r(get_field('days'), true)."</pre>");
+	?>
 
-			
+<?php
+wp_link_pages( array(
+	'before'      => '<div class="page-links">' . __( 'Pages:', 'twentyseventeen' ),
+	'after'       => '</div>',
+	'link_before' => '<span class="page-number">',
+	'link_after'  => '</span>',
+) );
+?>
 
-			<?php
-			/* Start the Loop */
+	
+
+	<?php
+	/* Start the Loop */
 /*			while ( have_posts() ) : the_post();
 
-				get_template_part( 'template-parts/post/content', get_post_format() );
+		get_template_part( 'template-parts/post/content', get_post_format() );
 
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
+		// If comments are open or we have at least one comment, load up the comment template.
+		if ( comments_open() || get_comments_number() ) :
+			comments_template();
+		endif;
 
-				the_post_navigation( array(
-					'prev_text' => '<span class="screen-reader-text">' . __( 'Previous Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Previous', 'twentyseventeen' ) . '</span> <span class="nav-title"><span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '</span>%title</span>',
-					'next_text' => '<span class="screen-reader-text">' . __( 'Next Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Next', 'twentyseventeen' ) . '</span> <span class="nav-title">%title<span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ) . '</span></span>',
-				) );
+		the_post_navigation( array(
+			'prev_text' => '<span class="screen-reader-text">' . __( 'Previous Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Previous', 'twentyseventeen' ) . '</span> <span class="nav-title"><span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '</span>%title</span>',
+			'next_text' => '<span class="screen-reader-text">' . __( 'Next Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Next', 'twentyseventeen' ) . '</span> <span class="nav-title">%title<span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ) . '</span></span>',
+		) );
 
-			endwhile; // End of the loop.*/
-			?>
+	endwhile; // End of the loop.*/
+	?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
-</div><!-- .wrap -->
+</div><!-- .container -->
 
 <?php get_footer();
